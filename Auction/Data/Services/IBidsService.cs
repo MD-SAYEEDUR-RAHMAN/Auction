aff337c36d67ec9bc762bd0d66d77f908ecd0aaa
MD-SAYEEDUR-RAHMAN <saidurnohan@gmail.com>
2024-05-13 14:31:47 +0600

    Complete

---
+++ Auction/Data/Services/IBidsService.cs
@@ -0,0 +1,11 @@
+﻿using Auction.Models;
+
+
+namespace Auction.Data.Services
+{
+    public interface IBidsService
+    {
+        Task Add(Bid bid);
+        IQueryable<Bid> GetAll();
+    }
+}
---
+++ Auction/Data/Services/ICommentsService.cs
@@ -0,0 +1,10 @@
+﻿using Auction.Models;
+
+
+namespace Auction.Data.Services
+{
+    public interface ICommentsService
+    {
+        Task Add(Comment comment);
+    }
+}
